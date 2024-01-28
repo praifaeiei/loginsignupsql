0cb8174b65958b6d576292ee39fedf7ddedc810d
praifaeiei <645051200089@mail.rmutk.ac.th>
2024-01-28 23:09:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/loginsignupsql/SignupActivity.kt
@@ -0,0 +1,49 @@
+package com.example.loginsignupsql
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.loginsignupsql.databinding.ActivitySignupBinding
+
+class SignupActivity : AppCompatActivity() {
+
+    class RegisterActivity : AppCompatActivity() {
+        private lateinit var binding: ActivitySignupBinding
+        private lateinit var databaseHelper: DatabaseHelper
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivitySignupBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            databaseHelper = DatabaseHelper(this)
+
+            binding.signupButton.setOnClickListener {
+                val  signupUsername = binding.signupEmail.text.toString()
+                val signupPassword = binding.signupPassword.text.toString()
+                signupDatabase(signupUsername, signupPassword)
+            }
+
+            binding.signupButton.setOnClickListener {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+        private  fun signupDatabase(username: String, password: String){
+            val insertedRowId = databaseHelper.insertUser(username, password)
+            if(insertedRowId != -1L){
+                Toast.makeText(this,"Signup Successful", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this,"Signup Failed",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
